001e3802b990b4e089e09d9dcc3b741a0a681890
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2025-10-17 08:24:42 -0300

    Initial commit

---
+++ Backend/main.py
@@ -0,0 +1,136 @@
+import flask
+import sqlite3
+import cryptography.fernet
+import os
+
+banco = sqlite3.connect("bancoprincipal.db", check_same_thread=False)
+comando = banco.cursor()
+
+key = cryptography.fernet.Fernet.generate_key()
+f = cryptography.fernet.Fernet(key)
+senhadoadmin = f.encrypt(b"admin")
+
+comando.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS usuario(
+        id INTEGER PRIMARY KEY,
+        nome TEXT NOT NULL,
+        senha TEXT NOT NULL,
+        Foto_de_perfil TEXT NOT NULL,
+        indentificação TEXT NOT NULL
+    )
+'''
+)
+
+if comando.execute('SELECT * FROM usuario').fetchall() == []:
+    comando.execute('INSERT INTO usuario (nome, senha, indentificação, Foto_de_perfil) VALUES (?, ?, ?, ?)', ('lying', 'gnr64puto1!', senhadoadmin.decode(), senhadoadmin.decode()))
+
+banco.commit()   
+
+app = flask.Flask(__name__, static_folder='./dist', static_url_path='/api')
+app.config['UPLOAD_FOLDER'] = 'static'
+
+
+@app.route('/')
+def serve():
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+def handler(request):
+    return app.wsgi_app(request.environ, request.start_response)
+
+@app.route('/<path:path>')
+def serve_static(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return flask.send_from_directory(app.static_folder, path)
+    else:
+        return flask.send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/login', methods=["POST"])
+def login():
+    data = flask.request.get_json()
+    nome = data.get("nome")
+    senha = data.get("senha")
+    print(nome, senha)
+    if(comando.execute('SELECT * FROM usuario WHERE nome = ? AND senha = ?', (nome, senha)).fetchall() != []):
+        indentificação = comando.execute('SELECT indentificação FROM usuario WHERE nome = ? AND senha = ?', (nome, senha)).fetchall()  
+        id = comando.execute('SELECT id FROM usuario WHERE nome = ? AND senha = ?', (nome, senha)).fetchall()  
+        return {"indentificação": indentificação[0][0],"id": id[0][0]}
+    else:
+        return "usuario não achado"
+
+@app.route('/api/cadrasto', methods=["GET", "POST"])
+def cadrasto():
+    if flask.request.method == "POST":
+        data = flask.request.get_json()
+        nome = data.get("nome")
+        senha = data.get("senha")
+        if(comando.execute('SELECT * FROM usuario WHERE nome = ?', (nome,)).fetchall() == []):
+            while True:
+                encriptar = os.urandom(16)
+                encripta = f.encrypt(b"" + encriptar)
+                if(comando.execute('SELECT * FROM usuario WHERE indentificação = ?', (encripta,)).fetchall() == []):
+                    comando.execute('INSERT INTO usuario (nome, senha, indentificação) VALUES (?, ?, ?)', (nome, senha, encripta.decode()))
+                    id = comando.execute('SELECT id FROM usuario WHERE nome = ? AND senha = ?', (nome, senha)).fetchall()
+                    banco.commit()
+                    return {"indentificação": encripta.decode(),"id": id[0][0]}
+                else:
+                    continue
+        else:
+            print("usuario ja existe")
+            return "usuario ja existe"
+
+@app.route('/api/enviar-mesnsagem', methods=["POST"])
+def enviar_mesnsagem():
+    data = flask.request.get_json()
+    mensagem = data.get("mensagem")
+    indentificação = data.get("idendificação")
+    indentificação = indentificação
+    print(indentificação, mensagem)
+    if(comando.execute('SELECT * FROM usuario WHERE indentificação = ?', (indentificação,)).fetchall() != []):
+        data = comando.execute('SELECT * FROM usuario WHERE indentificação = ?', (indentificação,)).fetchall()
+        print(data, mensagem)
+        comando.execute('''
+            CREATE TABLE IF NOT EXISTS mensagem(
+                id INTEGER PRIMARY KEY,
+                usuario_id INTEGER,
+                nome_de_usuario TEXT NOT NULL,
+                mensagem TEXT NOT NULL,
+                foto_de_perfil TEXT NOT NULL,
+                indetificação TEXT NOT NULL
+            )
+        '''
+        )
+        banco.commit()
+        comando.execute('INSERT INTO mensagem (usuario_id, nome_de_usuario, mensagem, indetificação, foto_de_perfil) VALUES (?, ?, ?, ?, ?)', (data[0][0], data[0][1], mensagem, indentificação, data[0][3],))
+        banco.commit()
+        return "mensagem enviada"
+    else:
+        return "usuario não achado"
+
+@app.route("/api/recebermensagem", methods=["GET"])
+def receber_mesnsagem():
+    comando.execute('SELECT usuario_id, mensagem, foto_de_perfil,nome_de_usuario FROM mensagem')
+    mensagens = comando.fetchall()
+    print(mensagens)
+    return {i: {"usuario_id": mensagens[i][0], "mensagem": mensagens[i][1], "fotodeperfil": mensagens[i][2], "nome": mensagens[i][3]} for i in range(len(mensagens))}
+
+@app.route('/api/getcontas', methods=['POST'])
+def getcontas():
+    data = flask.request.get_json()
+    indentificação = data.get("idendficação")
+    comando.execute('SELECT id, nome, Foto_de_perfil FROM usuario WHERE indentificação = ?', (indentificação,))
+    conta = comando.fetchall()
+    print(conta)
+    return conta
+
+@app.route('/api/enviar-imagem', methods=['POST'])
+def upload():
+    imagem = flask.request.files['imagem']
+    idendficação = flask.request.form['idendficação']
+    imagem.save(os.path.join(app.config['UPLOAD_FOLDER'], idendficação + '.png'))
+    comando.execute('UPDATE usuario SET Foto_de_perfil = ? WHERE indentificação = ?', (idendficação, idendficação))
+    banco.commit()
+    return "imagem recebida"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,169 @@
+import React, {useEffect, useState} from 'react'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import axios, {all, Axios} from "axios"
+import home from "./home"
+import profile from "./profile"
+import './App.css'
+
+function App() { 
+  const [islogin, setislogin] = useState()
+  const [id, setid] = useState()
+  const [perfil, setperfil] = useState()
+
+  useEffect(() => {
+    const thema = localStorage.getItem("theme") 
+    document.documentElement.setAttribute("data-theme", thema)
+    const login = localStorage.getItem("islogin")
+    const testar = async () => {
+      if(login == null){
+        setislogin(false)
+      }else{
+        setid(localStorage.getItem("id"))
+        setislogin(true)
+      }
+      if(thema == "light"){
+        document.getElementById("1").innerText = "dark"
+      }if(thema == "dark"){
+        document.getElementById("1").innerText = "light"
+      }
+    };testar();
+    const todascontas = () => {
+      const data = {
+        idendficação: localStorage.getItem("idendificação")
+      }
+        axios.post("/api/getcontas", data)
+          .then(Response => {
+            setperfil(Response.data[0][2])
+          })
+          .catch(erro => {
+            console.log(erro)
+          })
+    };todascontas()
+  }, []);
+  const mudarloginoucadrasto = (e) => {
+    if(e.target.id == "login.button"){
+      document.getElementById("login").hidden = true
+      document.getElementById("cadrasto").hidden = false
+      console.log("test")
+    }else{
+      document.getElementById("cadrasto").hidden = true
+      document.getElementById("login").hidden = false
+      console.log("test2")
+    }
+  }
+  const mudarthema = () => {
+    localStorage.setItem("theme", document.getElementById("1").innerText)
+    document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"))
+    if(document.getElementById("1").innerText == "light"){
+      document.getElementById("1").innerText = "dark"
+    }else{
+      document.getElementById("1").innerText = "light"
+    }
+  }
+  const certeza = (e) => {
+    if(e.target.id == "23"){
+      const data ={
+        nome: document.getElementById("login.user").value,
+        senha: document.getElementById("login.password").value
+      }
+      axios.post("/api/login", data)
+        .then((Response) => {
+          if(Response.data == "usuario não achado"){
+            console.log("deu erro")
+          }else{
+            console.log(Response.data)
+            localStorage.setItem("idendificação", Response.data.indentificação)
+            localStorage.setItem("id", Response.data.id)
+            localStorage.setItem("islogin", true)
+            setid(Response.data.id)
+            location.reload()
+          }
+        })
+        .catch(erro => {
+          console.log(erro)
+        });
+        
+    }else{  
+      const data ={
+        nome: document.getElementById("cadrasto.user").value,
+        senha: document.getElementById("cadrasto.password").value
+      }
+      axios.post("/api/cadrasto", data)
+        .then((Response) => {
+          if(Response.data == "usuario ja existe"){
+            console.log("deu erro")
+          }else{
+            localStorage.setItem("idendificação", Response.data.indentificação)
+            localStorage.setItem("id", Response.data.id)
+            setid(Response.data.id)
+            localStorage.setItem("islogin", true)
+            location.reload()
+          }
+        })
+        .catch(erro => {
+          console.log(erro)
+        });
+    }
+  }
+
+  return (
+    <>
+    <div>
+      {islogin ? (<BrowserRouter> 
+        <nav className='dark:bg-black border-2 bg-white border-black dark:border-white white flex justify-between items-center p-2'>
+          <div>
+            <Link className='justify-self-start' to={"/"}>
+              <img className='sm:w-10 w-8 rounded-full transition duration-300 ease-in-out hover:border-4 hover:border-purple-500' src="/chat_messages_14395.png"/>
+            </Link>
+          </div>
+          <div>
+            <img onClick={() => {document.getElementById("menu").hidden = false}} className='rounded-full sm:w-10 w-8 transition duration-300 ease-in-out hover:border-4 hover:border-purple-500 cursor-pointer' src={"/"+perfil+".png"}/>
+            <div className='absolute sm:right-1 sm:top-16 top-14 right-1 dark:bg-black border-2 bg-white border-black dark:border-white rounded-lg p-1 flex flex-col justify-center items-center' id='menu' hidden>
+              <p className='p-2 cursor-pointer transition duration-300 ease-in-out hover:border-purple-500 hover:border-1' onClick={() => {localStorage.clear(all) && location.reload(true)}}>Sair</p>
+              <Link to={"/profile/"+id}>
+                <p className='p-2 cursor-pointer transition duration-300 ease-in-out hover:border-purple-500 hover:border-1' onClick={() => {location.replace("/profile/"+id)}}>Perfil</p>
+              </Link>
+            </div>
+          </div>  
+        </nav>
+        <Routes>
+          <Route path='/' Component={home}/>
+          <Route path={"/profile/" + id} Component={profile}/>
+        </Routes>
+      </BrowserRouter>):(
+        <div className='flex flex-col h-screen items-center justify-center'>
+          <div className='dark:bg-black dark:border-white border-2 bg-white border-black rounded-xl p-4' id="login">
+            <h1>Login</h1>
+            <p>Usuario</p>
+            <input type="text" id="login.user" enterKeyHint={certeza}/>
+            <p>Senha</p>
+            <input type="password" id="login.password" enterKeyHint={certeza}/> <br />
+            <button id='23' type="submit" onClick={certeza}>Enviar</button>
+            <p className='max-w-28'>Se vocẽ não tiver uma conta, aperte o link abaixo</p>
+            <button className='text-blue-500' onClick={mudarloginoucadrasto} id='login.button'>cadrastar</button>
+          </div>
+          <div className='flex flex-col h-screen items-center justify-center' id="cadrasto" hidden>
+            <div className='dark:bg-black dark:border-white border-2 bg-white border-black rounded-xl p-4' id="cadrasto.2">
+              <h1>cadrasto</h1>
+              <p>Usuario</p>
+              <input type="text" id="cadrasto.user" enterKeyHint={certeza}/> 
+              <p>Senha</p>
+              <input type="password" id="cadrasto.password" enterKeyHint={certeza}/> 
+              <p>Senha de confirmação</p>
+              <input type="password" enterKeyHint={certeza}/><br />
+              <button id='32' type="submit" onClick={certeza}>Enviar</button>
+              <p className='max-w-28'>Se vocẽ tiver uma conta, aperte o link abaixo</p> 
+              <button className='text-blue-500' onClick={mudarloginoucadrasto} id='cadrasto.button'>logar</button>
+            </div>
+          </div>
+        </div>
+      )}
+      <button id="1" className="m-5 p-3 rounded-xl text-white bg-gray-700 fixed right-0 bottom-0 cursor-pointer transition duration-300 ease-in-out hover:border-purple-500 hover:border-4 hover:-translate-y-0.5" onClick={mudarthema}>
+        dark
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
