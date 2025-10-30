30f43bcf9912c576854c85e81d09b220e5a24d40
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2025-10-29 22:21:22 -0300

    1.5

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server:{
     proxy:{
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://chattr-16i4.onrender.com/', // Altere para o URL do seu backend ou site de destino
+        changeOrigin: true, // Garante que o cabeçalho 'Host' seja alterado para o destino
       },
     }
   }
