438e2998bee900d2825afa3745c9e8c65f596a93
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2025-10-29 22:59:21 -0300

    1.55

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,11 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],  
-  server:{
-    proxy:{
-      '/api': {
-        target: 'https://chattr-16i4.onrender.com', // Altere para o URL do seu backend ou site de destino
-      },
-    }
-  }
+  server: {
+    historyApiFallback: true,
+  },
 })
